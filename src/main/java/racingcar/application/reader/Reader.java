54cf552e3cafb8ffdaffca68d7b8cd9db8db581c
plll0123 <jsh951227@gmail.com>
2025-10-22 17:15:51 +0900

    refact: 사용자와 라운드를 입력받는 입출력 별도 클래스로 분리

--- src/main/java/racingcar/application/reader/Reader.java
+++ src/main/java/racingcar/application/reader/Reader.java
@@ -1,6 +1,5 @@
 package racingcar.application.reader;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.application.Car;
@@ -20,10 +19,8 @@ public class Reader {
     }
 
     Config doRead() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        int roundCount = Integer.parseInt(Console.readLine());
+        String carNames = Input.readCarNames();
+        int roundCount = Input.readRoundCount();
         return new Config(carNames, roundCount);
     }
 
