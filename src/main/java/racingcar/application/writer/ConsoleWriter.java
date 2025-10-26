7ad5d25824efcb00ae182200a26921502b1a56e2
plll0123 <jsh951227@gmail.com>
2025-10-26 18:13:23 +0900

    feat: 결과 출력시 문자열 추가

--- src/main/java/racingcar/application/writer/ConsoleWriter.java
+++ src/main/java/racingcar/application/writer/ConsoleWriter.java
@@ -4,6 +4,7 @@ public class ConsoleWriter implements Writer {
 
     @Override
     public void write(String result) {
+        System.out.println("\n실행 결과");
         System.out.println(result);
     }
 
