00048f9a29a97f4932995d4d5cd548a75045a088
plll0123 <jsh951227@gmail.com>
2025-10-26 18:09:18 +0900

    refact: TrackTest 불필요한 코드 제거

--- src/test/java/racingcar/application/TrackTest.java
+++ src/test/java/racingcar/application/TrackTest.java
@@ -3,35 +3,9 @@ package racingcar.application;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import org.assertj.core.api.Assertions;
-import org.assertj.core.api.AtomicBooleanAssert;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import racingcar.application.writer.Writer;
 
 class TrackTest {
-
-//    @ParameterizedTest
-//    @CsvSource(textBlock = """
-//            hun, 0, 'hun : '
-//            poby, 1, poby : -
-//            java, 2, java : --
-//            """)
-//    void 라운드별_출력_형식_검증(String carName, int mileage, String expectDisplay) {
-//        var car = new Car(carName, mileage);
-//        var raceInformation = new RaceInformation(1, List.of(car));
-//        var called = new AtomicBoolean();
-//        DisplayBoard displayBoard = new DisplayBoard("-", null) {
-//
-//        }
-//        Track track = new Track(new RandomProgressStrategy(), raceInformation, displayBoard);
-//        track.raceStart();
-//
-//        assertThat(car.getMileage()).isEqualTo(mileage);
-//        assertThat(displayBoard.getValue()).isEqualTo(expectDisplay);
-//    }
 
     @Test
     void 레이스_결과_검증() {
