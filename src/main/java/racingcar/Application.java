da146254d020bba718126719aec26b896c3cf346
plll0123 <jsh951227@gmail.com>
2025-10-26 16:58:03 +0900

    refact: 자동차 이동여부 계산 클래스 리팩토링 - 클래스 이름 오타 수정 - 클래스 접근 제어자 public으로 변경 - 패키지 이동

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import racingcar.application.RaceInformation;
+import racingcar.application.RandomProgressStrategy;
 import racingcar.application.Track;
 import racingcar.application.reader.Reader;
 import racingcar.application.writer.Writer;
@@ -9,7 +10,7 @@ public class Application {
 
     public static void main(String[] args) {
         RaceInformation raceInformation = new Reader().read();
-        Track track = new Track(new RandomProgressProgressStrategy(), raceInformation);
+        Track track = new Track(new RandomProgressStrategy(), raceInformation);
         track.raceStart();
         track.endRace();
         String result = track.getRecord();
---
+++ src/main/java/racingcar/application/RandomProgressStrategy.java
@@ -0,0 +1,16 @@
+package racingcar.application;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomProgressStrategy implements ProgressStrategy {
+
+    static final int MIN_NUMBER = 0;
+    static final int MAX_NUMBER = 9;
+    public static final int THRESHOLD = 4;
+
+    @Override
+    public boolean movable() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER) >= THRESHOLD;
+    }
+
+}
