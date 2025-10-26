167091f0e95ec4a4ee997ad554e353b98659327e
plll0123 <jsh951227@gmail.com>
2025-10-26 17:03:22 +0900

    refact: 클래스 구성요소 순서 변경

--- src/main/java/racingcar/application/ExceptionMessage.java
+++ src/main/java/racingcar/application/ExceptionMessage.java
@@ -2,11 +2,11 @@ package racingcar.application;
 
 public class ExceptionMessage {
 
+    public static final String CAR_NAME_TOO_LONG = "자동차 이름이 너무 깁니다.";
+    public static final String INVALID_RACER_NAME = "레이서 이름을 잘못 입력하였습니다.";
+
     private ExceptionMessage() {
         throw new UnsupportedOperationException("인스턴스 생성이 금지된 유틸 클래스입니다.");
     }
-
-    public static final String CAR_NAME_TOO_LONG = "자동차 이름이 너무 깁니다.";
-    public static final String INVALID_RACER_NAME = "레이서 이름을 잘못 입력하였습니다.";
 
 }
